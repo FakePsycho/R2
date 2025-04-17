5d999a792595d3c4bae7541bde92d90d9926b565
Miran2oe <miran777ooo@gmail.com>
2025-04-17 13:30:57 +0500

    zo'r

--- flutter_application_1/lib/ikki.dart
+++ flutter_application_1/lib/ikki.dart
@@ -10,6 +10,6 @@ class Ikki extends StatefulWidget {
 class _IkkiState extends State<Ikki> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Center(child: Text("hello word")));
   }
 }
